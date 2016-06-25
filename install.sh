1420a6bb8b82850d5f79348adbc3e574e9b05351
Chris Roy <christofer.roy@gmail.com>
2016-06-25 14:52:12 -0400

    First commit of dotfiles

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+dir="$( cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
+
+for FILEPATH in `find "$dir"/runcom/`; do
+	FILENAME=`basename $FILEPATH`
+	[ -f "$FILEPATH" ] && ln -sfv "$FILEPATH" ~/.$FILENAME
+done
