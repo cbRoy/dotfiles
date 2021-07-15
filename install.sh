9dac975139f87fd2ea28f64427f56ed7f718b05c
Chris Roy <christofer.roy@gmail.com>
2021-07-14 22:09:40 -0400

    some cleanup

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 
 dir="$( cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 
-for FILEPATH in `find "$dir"/runcom/`; do
-	FILENAME=`basename $FILEPATH`
-	[ -f "$FILEPATH" ] && ln -sfv "$FILEPATH" ~/.$FILENAME
-done
+#for FILEPATH in `find "$dir"/runcom/`; do
+#	FILENAME=`basename $FILEPATH`
+#	[ -f "$FILEPATH" ] && ln -sfv "$FILEPATH" ~/.$FILENAME
+#done
+
+
+ln -sfv $dir ~/.dotfiles
