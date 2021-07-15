0e3d5a65b057d88b5b6ec80810688006d13ec1ab
Chris Roy <christofer.roy@gmail.com>
2021-07-14 22:11:14 -0400

    oops

--- install.sh
+++ install.sh
@@ -2,10 +2,9 @@
 
 dir="$( cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 
-#for FILEPATH in `find "$dir"/runcom/`; do
-#	FILENAME=`basename $FILEPATH`
-#	[ -f "$FILEPATH" ] && ln -sfv "$FILEPATH" ~/.$FILENAME
-#done
-
+for FILEPATH in `find "$dir"/runcom/`; do
+	FILENAME=`basename $FILEPATH`
+	[ -f "$FILEPATH" ] && ln -sfv "$FILEPATH" ~/.$FILENAME
+done
 
 ln -sfv $dir ~/.dotfiles
